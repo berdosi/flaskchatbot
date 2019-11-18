7a14647005918f8a376637660c4ab9460287b93a
Balint <balint@radirpok>
2019-11-19 00:03:10 +0100

    add chattyness
    Signed-off-by: Balint <balint@radirpok>

--- interface/static/app.js
+++ interface/static/app.js
@@ -1,3 +1,25 @@
 (function () {
-	console.log("running");
+	const queryField = document.querySelector("#query");
+	const output = document.querySelector("#replies>dl");
+
+	queryField.addEventListener("keyup", (event) => {
+		if (event.key === "Enter") {
+			handleQuery();
+		}
+	});
+
+	function handleQuery() {
+		const query = queryField.value;
+
+		const feedback = document.createElement("dt");
+		const response = document.createElement("dd");
+
+		output.prepend(feedback, response);
+		feedback.innerText = query;
+
+		fetch("/api/query/" + encodeURIComponent(query))
+			.then(responseObject => responseObject.json())
+			.then(value => response.innerText = JSON.stringify(value))
+			.catch((e) => response.innerText = e);
+	}
 })()
